5c159e5442ac0f1c5c6b160ca4bb39be123d0c1a
Joaquin Popoca <joaquin.popoca@wizeline.com>
2023-10-26 17:07:12 -0600

    refactor: add exports aggregation file to models

---
+++ src/models/QuizInfo.ts
@@ -0,0 +1,11 @@
+type QuizInfo = {
+    id: string,
+    name: string,
+    image?: string,
+    creationDate?: string,
+    updatedDate?: string,
+    availableDate?: string,
+    notesDocumentId?: string,
+}
+
+export default QuizInfo;
