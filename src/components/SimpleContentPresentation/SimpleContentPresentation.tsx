0d9958937c2df0e6a47f1dc3ecd20e289104ccc7
Joaquin Popoca <joaquin.popoca@wizeline.com>
2024-02-15 14:24:49 -0600

    add: SimpleContentPresentation component
    A new component named SimpleContentPresentation has been created and integrated into QuizComponentSelector.tsx. This component's purpose is to render simple text content in a defined and reusable format, improving both readability and coherence within the application.

---
+++ src/components/SimpleContentPresentation/SimpleContentPresentation.tsx
@@ -0,0 +1,7 @@
+import SimpleContentPresentationItem from "./types.ts";
+
+const SimpleContentPresentation = ({ content }: SimpleContentPresentationItem) => {
+    return <h3>{content}</h3>;
+}
+
+export default SimpleContentPresentation;
---
+++ src/components/SimpleContentPresentation/index.ts
@@ -0,0 +1,2 @@
+export { default } from './SimpleContentPresentation.tsx';
+export type { default as SimpleContentPresentationItem } from './types.ts';
